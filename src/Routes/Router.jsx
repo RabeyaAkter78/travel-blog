08b508d5b7bb930a0c25a3ce21274b08f54d1370
RabeyaAkter78 <rabeyariya251@gmail.com>
2024-01-01 21:20:11 +0600

    added navbar

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -3,23 +3,31 @@ import Home from "../components/Home/Home";
 import Blogs from "../components/Blogs/Blogs";
 import AboutUs from "../components/AboutUs/AboutUs";
 import ContactUs from "../components/ContactUs/ContactUs";
+import Main from "../components/Main/Main";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home></Home>,
+        element: <Main></Main>,
+        children: [
+            {
+                path: "/",
+                element: "Home"
+            },
+            {
+                path: "blogs",
+                element: <Blogs></Blogs>
+            },
+            {
+                path: "aboutUs",
+                element: <AboutUs></AboutUs>
+            },
+            {
+                path: "contactUs",
+                element: <ContactUs></ContactUs>
+            }
+        ]
     },
-    {
-        path: "/blogs",
-        element: <Blogs></Blogs>
-    },
-    {
-        path: "/aboutUs",
-        element: <AboutUs></AboutUs>
-    },
-    {
-        path: "/contactUs",
-        element: <ContactUs></ContactUs>
-    }
+
 
 ]);
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,13 +9,17 @@ const NavBar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><Link to="/">Home</Link></li>
+                        <li><Link to={"home"}>Home</Link></li>
                         <li><Link to="/blogs">Blogs</Link></li>
                         <li><Link to="/aboutUs">About Us</Link></li>
-                        <li><Link to="contactUs">Contact Us</Link></li>
+                        <li><Link to="/contactUs">Contact Us</Link></li>
+                        {/* <li><Link to="/">Home</Link></li>
+                        <li><Link to="/blogs">Blogs</Link></li>
+                        <li><Link to="/aboutUs">About Us</Link></li>
+                        <li><Link to="/contactUs">Contact Us</Link></li> */}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Blogs</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
